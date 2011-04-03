d4959092c19982744c3e166f3fc75ea84a484eba
damlaok <damla.ok.90@gmail.com>
2011-04-03 14:06:48 +0000

    ScrollBar saga tasindi

---
+++ playground/damla.ok/cink.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import Qt
+import QTermWidget
+
+a = Qt.QApplication(sys.argv)
+w = QTermWidget.QTermWidget()
+
+w.setScrollBarPosition(2)
+w.show()
+a.exec_()
