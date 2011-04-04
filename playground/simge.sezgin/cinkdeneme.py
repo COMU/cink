a24d64a28e8b41af312cee48444e28928074a257
simgeszgn <simgesezgin88@gmail.com>
2011-04-04 22:43:21 +0000

    arka plan renk seçimi kısayol tuşlarla

--- playground/simge.sezgin/cinkdeneme.py
+++ playground/simge.sezgin/cinkdeneme.py
@@ -3,13 +3,22 @@
 
 import sys
 from PyQt4 import Qt
+from PyQt4.QtGui import QShortcut
 import QTermWidget
 
 a=Qt.QApplication(sys.argv)
 w=QTermWidget.QTermWidget()
 
-w.setColorScheme(2)
+def gri():
+    w.setColorScheme(1)
+def yesil():
+    w.setColorScheme(2)
+def sari():
+    w.setColorScheme(3)
 
+kisayol1=QShortcut("g",w,gri)
+kisayol2=QShortcut("y",w,yesil)
+kisayol3=QShortcut("s",w,sari)
 w.setScrollBarPosition(2)
 
 w.show()
