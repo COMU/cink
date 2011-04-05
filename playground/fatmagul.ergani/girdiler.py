a9b71cc4ad9f93db4db8bed276eeb09352e510fe
simgeszgn <simgesezgin88@gmail.com>
2011-04-05 13:19:49 +0000

    gereksiz cink dosyası silindi

--- playground/fatmagul.ergani/girdiler.py
+++ playground/fatmagul.ergani/girdiler.py
@@ -8,12 +8,11 @@ import QTermWidget
 
 while True:
     a = raw_input (u"lütfen girmek için c ye basınız: ")
-    if a=='c':
+    if a=='/141':
         a = Qt.QApplication(sys.argv)
         w = QTermWidget.QTermWidget()
 
         w.show()
         a.exec_()
-
         break
 
--- playground/simge.sezgin/cinkdeneme.py
+++ playground/simge.sezgin/cinkdeneme.py
@@ -9,16 +9,17 @@ import QTermWidget
 a=Qt.QApplication(sys.argv)
 w=QTermWidget.QTermWidget()
 
+def sari():
+    w.setColorScheme(3)
 def gri():
     w.setColorScheme(1)
 def yesil():
     w.setColorScheme(2)
-def sari():
-    w.setColorScheme(3)
 
-kisayol1=QShortcut("g",w,gri)
-kisayol2=QShortcut("y",w,yesil)
-kisayol3=QShortcut("s",w,sari)
+w.setColorScheme(2)
+kisayol1=QShortcut("Alt+g",w,gri)
+kisayol2=QShortcut("ALT+y",w,yesil)
+kisayol3=QShortcut("ALT+s",w,sari)
 w.setScrollBarPosition(2)
 
 w.show()
