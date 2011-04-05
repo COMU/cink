9f28a9f1659bd1f7bae89233dd39aca06ecd4086
simgeszgn <simgesezgin88@gmail.com>
2011-04-05 13:43:02 +0000

    cink eklendi

---
+++ proje/cink.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import Qt
+from PyQt4.QtGui import QShortcut
+import QTermWidget
+
+a = Qt.QApplication(sys.argv)
+w = QTermWidget.QTermWidget()
+
+def sari():
+    w.setColorScheme(3)
+def gri():
+    w.setColorScheme(1)
+def yesil():
+    w.setColorScheme(2)
+
+w.setColorScheme(2)
+kisayol1=QShortcut("Alt+g",w,gri)
+kisayol2=QShortcut("ALT+y",w,yesil)
+kisayol3=QShortcut("ALT+s",w,sari)
+
+w.setScrollBarPosition(2)
+
+w.show()
+a.exec_()
