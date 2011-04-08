fe8de667c2b0b8676aa3b667b3066f049d83154e
fatmagulergani <fatmagul90@gmail.com>
2011-04-08 13:38:20 +0000

    sag tiklamayi QShortcut'la yapmaya çalıştım

---
+++ playground/fatmagul.ergani/tiklama.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import sys
+from PyQt4 import QtGui
+from PyQt4.QtGui import QShortcut
+import QTermWidget
+from PyQt4 import QtCore
+from PyQt4 import Qt
+
+class Ana(QTermWidget.QTermWidget):
+    
+    def __init__(self):
+        super(Ana, self).__init__()
+
+        self.resize(500,500)
+        self.setWindowTitle('konsol')
+
+   # def mousePressEvent(self,event):
+       # print "aaa"
+        #if event.button() == QtCore.Qt.RightButton:
+           # print "oldu"
+def onRightClick(event):
+    print "aaa"
+    showPosEvent(event)
+
+def kapatma():
+    app.exit()
+
+
+
+if __name__=='__main__':
+    app = QtGui.QApplication(sys.argv)
+    widget = Ana()
+   # widget = QTermWidget.QTermWidget()
+   # widget.resize(500, 500)
+   # widget.setWindowTitle('konsol')
+    shortcut1=QShortcut("@",widget,kapatma)
+    sss=QShortcut("<Button-3>",widget,onRightClick)
+    widget.setScrollBarPosition(2)
+    widget.setColorScheme(2)
+
+    widget.show()
+    sys.exit(app.exec_())
