c2c06d56804eca575b3140b6ab9e0b20f1988894
simgeszgn <simgesezgin88@gmail.com>
2011-04-25 21:15:30 +0000

    aynı tuş ile ekranı büyütüp küçültme

--- proje/boyut.py
+++ proje/boyut.py
@@ -15,20 +15,14 @@ def gri():
     w.setColorScheme(1)
 def yesil():
     w.setColorScheme(2)
-def tamekran():
-    w.showFullScreen()
-def eskiekran():
-    w.showNormal()
+def ekran():
+    if(w.isFullScreen()==False):
+        w.showFullScreen()
+    else:
+        w.showNormal()
+
 def kapama():
     w.close()
-def ekran():
-    while(True):
-        kisayol7=QShortcut("SHIFT+t",w,tamekran)
-        i=1
-        if(i==1):
-            kisayol8=QShortcut("SHIFT+t",w,eskiekran)
-            break
-
 
 w.resize(600,400)
 w.setColorScheme(2)
@@ -37,8 +31,7 @@ kisayol1=QShortcut("Alt+g",w,gri)
 kisayol2=QShortcut("ALT+y",w,yesil)
 kisayol3=QShortcut("ALT+s",w,sari) 
 kapat=QShortcut("ALT+c",w,kapama)
-kisayol4=QShortcut("SHIFT+a",w,ekran)
-# kisayol7=QShortcut("SHIFT+t",w,tamekran)
+kisayol7=QShortcut("SHIFT+t",w,ekran)
 w.setColorScheme(2) 
 
 
--- proje/cink.py
+++ proje/cink.py
@@ -15,21 +15,29 @@ def gri():
     w.setColorScheme(1)
 def yesil():
     w.setColorScheme(2)
-def tamekran():
-    w.resize(2048,2048)
-def tam_ekran():
-    w.showFullScreen()
-def normal_ekran():
-    w.showNormal()
+#def tamekran():
+  #  w.resize(2048,2048)
+#def tam_ekran():
+  #  w.showFullScreen()
+#def normal_ekran():
+  #  w.showNormal()
+'aynı tuşu kullanarak konsolu büyütüp küçültme'
+def ekran():
+    if(w.isFullScreen()==False):
+        w.showFullScreen()
+    else:
+        w.showNormal()
 
+def kapama():
+    w.close()
 w.setColorScheme(2)
 
 kisayol1=QShortcut("Alt+g",w,gri)
 kisayol2=QShortcut("ALT+y",w,yesil)
 kisayol3=QShortcut("ALT+s",w,sari)
-kisayol4=QShortcut("SHIFT+CTRL+F11",w,tamekran)
-kisayol5=QShortcut("Ctrl+f",w,tam_ekran)
-kisayol6=QShortcut("Ctrl+n",w,normal_ekran)
+# kisayol4=QShortcut("SHIFT+CTRL+F11",w,tamekran)
+kisayol6=QShortcut("Ctrl+F11",w,ekran)
+kapat=QShortcut("ALT+c",w,kapama)
 
 w.setScrollBarPosition(2)
 
