be8335fbee2a73ae7926e740b84f9060b45b97cb
fatmagulergani <fatmagul90@gmail.com>
2011-04-27 10:44:55 +0000

    transparanlık

--- playground/fatmagul.ergani/girdi.py
+++ playground/fatmagul.ergani/girdi.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import sys
 from PyQt4 import QtGui
 from PyQt4.QtGui import QShortcut
 import QTermWidget
 from PyQt4 import QtCore
 from PyQt4 import Qt
-
-class Ana(QTermWidget.QTermWidget):
-    
-    def __init__(self):
-        super(Ana, self).__init__()
-
-        self.resize(500,500)
-        self.setWindowTitle('konsol')
-
-    def mousePressEvent(self,event):
-        print "aaa"
-        if event.button() == QtCore.Qt.RightButton:
-            print "oldu"
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QPixmap
+from PyQt4.Qt import *
 
 def kapatma():
     app.exit()
 
+def transparent():
+    widget.setWindowOpacity(0.6)
 
 
 if __name__=='__main__':
     app = QtGui.QApplication(sys.argv)
-    widget = Ana()
-   # widget = QTermWidget.QTermWidget()
-   # widget.resize(500, 500)
-   # widget.setWindowTitle('konsol')
-    shortcut1=QShortcut("@",widget,kapatma)
+    widget = QTermWidget.QTermWidget()
+    #widget.setStyleSheet("background-color: rgba(255, 255, 255, 0%);")
+    #widget.setWindowOpacity(0.5)
+    #widget.setBackgroundRole(QtGui.QPalette.Base)
+    #widget.setAttribute(Qt.WA_NoSystemBackground, True)
+    #widget.setAttribute(Qt.WA_TranslucentBackground)
+    widget.resize(500, 500)
+    widget.setWindowTitle('konsol')
+    shortcut1=QShortcut("Ctrl+c",widget,kapatma)
+    shortcut2=QShortcut("@",widget,transparent)
     widget.setScrollBarPosition(2)
     widget.setColorScheme(2)
 
     widget.show()
     sys.exit(app.exec_())
+
