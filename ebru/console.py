c57d4e52911e2c28d9f35c121d2eb367e401d10b
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-10-13 22:53:35 +0300

    F6 tusuna basıldığında uyarı veren fonksiyon eklendi

--- ebru/console.py
+++ ebru/console.py
@@ -9,10 +9,17 @@ class Console:
 		terminal.fork_command('bash')
 		terminal.connect('event',self.right_click)
 		win = gtk.Window()
-		terminal.set_background_transparent(45000)
+		terminal.set_background_transparent(1)
 	        win.add(terminal)
+		win.connect('key-press-event',self.shortcut)
 	        win.connect('delete-event', lambda win, event: gtk.main_quit())
-	        win.show_all()
+	        win.show_all()	
+
+	def shortcut(self, widget, event, *args):
+                if event.keyval == gtk.keysyms.F6:
+                        print "F6 ya basildi"
+
+
 	def right_click(self,terminal,event):
 		if event.type == gtk.gdk.BUTTON_PRESS:
 	            if event.button != 3:
