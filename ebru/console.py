1e9859bc40b75e9a562d92475d2c9c6291c7f0c4
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-10-21 15:28:31 +0300

    gereksiz satırlar çıkarılıp boyut pepencere boyutu değiştirildi

--- ebru/console.py
+++ ebru/console.py
@@ -1,33 +1,22 @@
 import gtk 
 import vte
+import os
 
-#terminal = vte.Terminal()
 win = gtk.Window()
-notebook = gtk.Notebook()
-class Console(vte.Terminal,gtk.Notebook,gtk.Window):
+class Console(vte.Terminal,gtk.Window):
 
 	def __init__(self):	 	
         
 		self.terminal = vte.Terminal()
 	        self.is_fullscreen = False
-		self.terminal.fork_command('bash')	
-		win.resize(1000,3000)
+		self.terminal.fork_command()
+		win.resize(100,30)
 		self.terminal.set_background_transparent(1)  # set_background_transparent boolean degerler aliyo
 	        win.connect('delete-event', lambda win, event: gtk.main_quit())
 		win.connect('key-press-event',self.full_screen)
 	        self.terminal.connect('event',self.right_click)
 		win.add(self.terminal)
                 win.show_all()
-
-	def _create_terminal(self, widget, data=None):
-		print "q"
-
-	def quit_cb(self):
-		print "ss"
-#		keyval = gtk.gdk.keyval_from_name
-		print "a"
-		print "degil"
-
 	
 	def pageSelected(self, notebook, page, pagenum):
 		name = notebook.get_tab_label(notebook.get_nth_page(pagenum))
@@ -54,7 +43,6 @@ class Console(vte.Terminal,gtk.Notebook,gtk.Window):
 			item2.connect("activate",self.paste)
 			item3 = gtk.MenuItem("Close Tab")
 			item4 = gtk.MenuItem("Quit")	
-#			item4.connect("activate",self.quit)	
 			item4.connect("activate", lambda discard: gtk.main_quit())
 			item1.show()
 			m.append(item1)
@@ -65,8 +53,6 @@ class Console(vte.Terminal,gtk.Notebook,gtk.Window):
 			item4.show()
                         m.append(item4)
            		m.popup(None, None, None, event.button, event.time, None)
-	def quit():
-		gtk.main_quit()
 		
 	def copy(self, widget=None, data=None):
 		if self.terminal.get_has_selection():
