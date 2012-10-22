9d47f6bdf481b4df7d2e3eace805e45d3f9ad928
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-10-22 23:21:50 +0300

    Birden fazla sekme açtırma işlemi fonksiyonun içine atıldı. İlerki zamanlarda kod tekrarı olmaması için kodlar fonksiyonlara paylaştırıldı.

--- ebru/console.py
+++ ebru/console.py
@@ -8,36 +8,44 @@ class Console(vte.Terminal):
 
 	def __init__(self):	 	
         
-	        self.is_fullscreen = False
 		win.resize(400,400)
 	        win.connect('delete-event', lambda win, event: gtk.main_quit())
 		win.connect('key-press-event',self.full_screen)
 		self.notebook = gtk.Notebook()
-		win.add(self.notebook)
-		for i in range(1,3):
-			vbox = gtk.VBox(False, 0)
-			label = gtk.Label("tab"+str(i))
-               		vbox.pack_start(label)
-	                vbox.show_all()
-			argv = ['bash']
-			env = self.env_map_to_list(os.environ.copy())
-			cwd = os.environ['HOME']
-			self.terminal = vte.Terminal()
-			self.terminal.fork_command(argv[0], argv, env, cwd)
-			self.terminal.set_background_transparent(1)  # set_background_transparent boolean degerler aliyo
-			self.notebook.insert_page(self.terminal,vbox)
-		
-	        self.terminal.connect('event',self.right_click)
+		win.add(self.notebook)	
+		self.create_tab()
                 win.show_all()
+
+	def create_terminal(self):
+		self.argv = ['bash']
+		self.env = self.env_map_to_list(os.environ.copy())
+		self.cwd = os.environ['HOME']
+		self.is_fullscreen = False
+		self.terminal = vte.Terminal()
+		self.terminal.set_background_transparent(1)
+
+	def terminal_action(self):
+		self.create_terminal()
+                self.terminal.fork_command(self.argv[0], self.argv, self.env, self.cwd)
+		self.terminal.connect('event',self.right_click)
+
+	def create_tab(self):
+		for i in range(1,3):
+                        vbox = gtk.VBox(False, 0)
+                        label = gtk.Label("tab"+str(i))
+                        vbox.pack_start(label)
+                        vbox.show_all()
+			self.terminal_action()
+                        self.notebook.insert_page(self.terminal,vbox)
 
  	def env_map_to_list(self, env): # terminal fork_command icin
 		return ['%s=%s' % (k, v) for (k, v) in env.items()]
+
 	def pageSelected(self, notebook, page, pagenum):
 		name = notebook.get_tab_label(notebook.get_nth_page(pagenum))
 		if name == 'tab1':	
 			print "-"
-			print notebook.get_nth_page(pagenum)
-		
+			print notebook.get_nth_page(pagenum)		
 
 	def full_screen(self, widget, event):
  		 if  event.keyval == gtk.keysyms.F11:
@@ -47,6 +55,7 @@ class Console(vte.Terminal):
                         elif self.is_fullscreen == True:
                                 win.unfullscreen()
                                 self.is_fullscreen = False
+
 	def right_click(self,widget,event):
 		if event.type == gtk.gdk.BUTTON_PRESS:
 	            if event.button == 3:    # 1 sol tus,2 orta tus icin
@@ -71,6 +80,7 @@ class Console(vte.Terminal):
 	def copy(self, widget=None, data=None):
 		if self.terminal.get_has_selection():
 			self.terminal.copy_clipboard()
+
 	def paste(self, widget=None, data=None):
 		self.terminal.paste_clipboard()
 
