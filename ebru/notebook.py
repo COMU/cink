bc459a333b2667cda4794ccd9a7f51dfab0b6dde
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-01-09 10:51:33 +0200

    Gereksiz yorum satırı halindeki kodlar silindi

--- ebru/notebook.py
+++ ebru/notebook.py
@@ -6,28 +6,6 @@ import time
 from utils import Utils
 from vteTerminal import VteTerminal
 
-"""class VteTerminal(Utils):
-	def constr(self):
-		self.terminal = []
-		self.index_ = -1
-
-	def terminal_setting(self):
-		self.argv = ['bash']
-	        self.env = self.env_map_to_list(os.environ.copy())
-	        self.cwd = os.environ['HOME']
-	        self.is_fullscreen = False
-	        self.terminal.append(vte.Terminal())
-	        # self.index_ sekme sayfasiyla ayni
-	        self.terminal[self.index_].set_colors(gtk.gdk.color_parse('white'),gtk.gdk.color_parse('pink'),[])
-	        #self.terminal.set_background_transparent(1)
-
-	def terminal_action(self):
-	        self.terminal[self.index_].fork_command(self.argv[0], self.argv, self.env, self.cwd)
-	        return self.terminal[self.index_]
-
-	def env_map_to_list(self, env): # terminal fork_command icin
-	        return ['%s=%s' % (k, v) for (k, v) in env.items()]
-"""
 class Console(Utils,VteTerminal):
     def __init__(self):
 	self.win = gtk.Window()
